cb6f5626d6463692865233886861bc5c62f7608a
NOR <inlight.ur.life@gmail.com>
2019-01-23 18:13:50 +0000

    shopping cart added

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'search_app',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -55,14 +57,21 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'shop', 'templates/'),
+            os.path.join(BASE_DIR, 'search_app', 'templates/'),
+            os.path.join(BASE_DIR, 'cart', 'templates/'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -13,6 +13,8 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_url(self):
+        return reverse('shop:products_by_category', args=[self.slug])
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -35,6 +37,10 @@ class Product(models.Model):
         ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+
+    def get_url(self):
+        return reverse('shop:ProdCatDetail', args=[self.category.slug, self.slug])
 
 
     def __str__(self):
